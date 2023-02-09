87481f04cfdff3b2d15c833d0563d6501c69ef2f
TerminiUsMag <nikolaidan1@abv.bg>
2023-02-09 22:26:05 +0200

    01. - 11. All done 100/100 :) Under 10 minutes

---
+++ 04. Built-in Functions/Exercises.sql
@@ -0,0 +1,46 @@
+--01.--SELECT FirstName, LastName FROM Employees WHERE FirstName LIKE 'Sa%'
+
+--02.--SELECT FirstName, LastName FROM Employees WHERE LastName LIKE '%ei%'
+
+--03.--SELECT FirstName 
+--  FROM Employees
+--  WHERE (DepartmentID = 3 OR DepartmentID = 10)
+--        AND
+--        (YEAR(HireDate)>=1995 AND YEAR(HireDate)<= 2005)
+
+--04.--SELECT FirstName,LastName FROM Employees WHERE JobTitle NOT LIKE '%engineer%'
+
+--05.--SELECT [Name] FROM Towns WHERE LEN([Name]) = 5 OR LEN([Name]) = 6 ORDER BY [Name]
+
+--06.--SELECT * FROM Towns WHERE [Name] LIKE '[MKBE]%' ORDER BY [Name]
+
+--07.--SELECT * FROM Towns WHERE [Name] NOT LIKE '[RBD]%' ORDER BY [Name]
+
+--08.--CREATE VIEW V_EmployeesHiredAfter2000 AS
+     --SELECT FirstName,LastName
+     --  FROM Employees
+     -- WHERE YEAR(HireDate) > 2000
+
+--09.--SELECT FirstName,LastName
+--  FROM Employees
+-- WHERE LEN(LastName) =5
+
+--10.--SELECT EmployeeID
+--      ,FirstName
+--	  ,LastName
+--	  ,Salary
+--	  ,DENSE_RANK() OVER (PARTITION BY Salary ORDER BY EmployeeID) 
+--  FROM Employees
+-- WHERE Salary BETWEEN 10000 AND 50000
+-- ORDER BY Salary DESC
+
+--11.--SELECT * FROM (SELECT EmployeeID
+--      ,FirstName
+--	  ,LastName
+--	  ,Salary
+--	  ,DENSE_RANK() OVER (PARTITION BY Salary ORDER BY EmployeeID) AS [Rank]
+--  FROM Employees
+-- WHERE Salary BETWEEN 10000 AND 50000) AS s
+-- WHERE s.[Rank] = 2
+-- ORDER BY s.Salary DESC
+
